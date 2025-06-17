7930008996d1073e366a0272bfd8d76a2e1a3b5d
S Nitheesh <qxnitheesh@gmail.com>
2025-06-17 20:57:11 +0530

    Refactor appwrite client setup and enhance LoginPage with error handling and form submission

--- src/appwrite/appwrite.js
+++ src/appwrite/appwrite.js
@@ -3,7 +3,7 @@ import { APPWRITE_ENDPOINT, APPWRITE_PROJECT_ID } from "../../appwriteConfig";
 
 export const client = new Client();
 
-client.setEndpoint(`${APPWRITE_ENDPOINT}`).setProject(`${APPWRITE_PROJECT_ID}`);
+client.setEndpoint(APPWRITE_ENDPOINT).setProject(APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export { ID } from "appwrite";
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,33 +1,61 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { User } from "lucide-react";
 import { LockKeyhole } from "lucide-react";
 import { CircleUserRound } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { account } from "../appwrite/appwrite";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await account.createEmailPasswordSession(email, password);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  console.log(error);
   return (
     <div className="md:w-[500px]  mx-auto md:border-2 mt-20 rounded-xl shadow-md flex flex-col gap-10 py-5 md:bg-[#00292D]">
+      {error && (
+        <p className="text-red-500 text-center font-semibold">{error}</p>
+      )}
       <h1 className="font-semibold flex items-center flex-col text-white text-2xl cursor-pointer">
         <CircleUserRound size={100} />
         USER LOGIN
       </h1>
-      <form className="w-full px-10 flex flex-col gap-10 mt-5">
+      <form
+        className="w-full px-10 flex flex-col gap-10 mt-5"
+        onSubmit={handleLogin}
+      >
         <div className="flex gap-2 rounded-full bg-gray-200/20 backdrop-blur-md border-gray-300/30 py-3 px-5 text-slate-200">
           <User />
           <input
-            type="text"
+            type="email"
             name="user"
             id="user"
             className="bg-transparent flex-1 outline-none text-md"
             placeholder="Username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="flex gap-2 rounded-full bg-gray-200/20 backdrop-blur-md border-gray-300/30 py-3 px-5 text-slate-200">
           <input
             type="password"
-            name="user"
-            id="user"
+            name="pass"
+            id="pass"
             className="bg-transparent flex-1 outline-none text-md"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <LockKeyhole />
         </div>
@@ -42,7 +70,7 @@ const LoginPage = () => {
           </a>
         </div>
         <div className="mx-auto flex justify-between w-full items-center">
-          <p href="" className="text-white font-semibold cursor-pointer">
+          <p className="text-white font-semibold cursor-pointer">
             Create Account
           </p>
           <input
